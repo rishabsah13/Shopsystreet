e3046178cb9fd00f3176ee63a6ce66178ff6eb1d
rishabsah13 <eparis@atlassian.com>
2024-07-09 22:47:14 +0530

    frontend

--- src/api.js
+++ src/api.js
@@ -9,11 +9,9 @@ export const getProduct = (id) => api.get(`/products/${id}`);
 export const createProduct = (product) => api.post('/products', product);
 export const updateProduct = (id, product) => api.put(`/products/${id}`, product);
 export const deleteProduct = (id) => api.delete(`/products/${id}`);
-
 export const getOrders = () => api.get('/orders');
 export const getOrder = (id) => api.get(`/orders/${id}`);
 export const createOrder = (order) => api.post('/orders', order);
-
 export const createPaymentIntent = (items) => api.post('/payments/create-payment-intent', { items });
 export const confirmPayment = (paymentIntentId, orderId) => api.post('/payments/confirm-payment', { paymentIntentId, orderId });
 
