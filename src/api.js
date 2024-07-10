25033ca43195230fe9bddff96fc69ddb2116a619
rishabsah13 <106971462+rishabsah13@users.noreply.github.com>
2024-07-10 23:21:21 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://shopsystreet-3.vercel.app/'
 });
 
 export const getProducts = () => api.get('/products');
