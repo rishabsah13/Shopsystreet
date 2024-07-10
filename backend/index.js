c8cedab629d46baf729323adde81834423261376
rishabsah13 <106971462+rishabsah13@users.noreply.github.com>
2024-07-10 23:35:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ const cors = require("cors")
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://shopsystreet-fe.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+))
 app.use(bodyParser.json());
 
 sequelize.authenticate()
