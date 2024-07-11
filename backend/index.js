0e962674eade654ca3cf903cca164ff9f2fd2167
rishabsah13 <106971462+rishabsah13@users.noreply.github.com>
2024-07-12 01:30:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT||3000;
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["*"],
   methods: ["POST", "GET", "PATCH", "DELETE", "PUT"],
   credentials: true
 }));
